658d756b3e4ef8ee774ca28c43485f98af90ae27
Lauren Pineiro <lamor1800@gmail.com>
2022-10-29 16:15:06 -0400

    first commit

---
+++ client/src/js/index.js
@@ -0,0 +1,46 @@
+import { Workbox } from 'workbox-window';
+import Editor from './editor';
+import './database';
+import '../css/style.css';
+
+//webpackage install 
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+const main = document.querySelector('#main');
+main.innerHTML = '';
+
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
+
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
+
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+}
+
