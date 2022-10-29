e83e7888743e0bfbf0c43399ed00236b89578e61
Lauren Pineiro <lamor1800@gmail.com>
2022-10-29 17:36:13 -0400

    removed webpack install function from index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,18 +3,6 @@ import Editor from './editor';
 import './database';
 import '../css/style.css';
 
-//webpackage install 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
-
 const main = document.querySelector('#main');
 main.innerHTML = '';
 
