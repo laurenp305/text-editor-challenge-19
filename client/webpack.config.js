9374172b27a19d6c920e878de27916713018772b
Lauren Pineiro <lamor1800@gmail.com>
2022-11-01 18:22:25 -0400

    added css loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,21 +5,16 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 plugins: [
-  // Webpack plugin that generates our html file and injects our bundles. 
   new HtmlWebpackPlugin({
-   // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
    template: './index.html', 
-   // Optional parameters
    title: 'Text Editor'
  }),
 
- // Injects our custom service worker
  new InjectManifest({
    swSrc: './src-sw.js',
    swDest: 'src-sw.js',
  }),
 
- // Creates a manifest.json file.
  new WebpackPwaManifest({
    fingerprints: false,
    inject: true,
@@ -39,70 +34,25 @@ plugins: [
    ],
  }),
  ],
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+module: {
+  // TODO: Add CSS loaders and babel to webpack.
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"]
     },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Just Another Text Editor'
-      }),
-      // TODO: Add and configure workbox plugins for a service worker and manifest file.
-      // service worker
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
-      // manifest.json
-      new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'Just Another Text Editor!',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-    ],
-
-    module: {
-      // TODO: Add CSS loaders and babel to webpack.
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
-            },
-          },
-        },
-      ],
-    },
-  };
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+        }
+      }
+    }
+  ],
+},
+};
 };
