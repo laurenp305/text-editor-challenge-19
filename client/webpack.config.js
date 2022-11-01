d5c2cfb7aff163963b6b911008691054c729fb01
Lauren Pineiro <lamor1800@gmail.com>
2022-11-01 18:20:31 -0400

    moved the info from package json to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,41 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+plugins: [
+  // Webpack plugin that generates our html file and injects our bundles. 
+  new HtmlWebpackPlugin({
+   // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
+   template: './index.html', 
+   // Optional parameters
+   title: 'Text Editor'
+ }),
+
+ // Injects our custom service worker
+ new InjectManifest({
+   swSrc: './src-sw.js',
+   swDest: 'src-sw.js',
+ }),
+
+ // Creates a manifest.json file.
+ new WebpackPwaManifest({
+   fingerprints: false,
+   inject: true,
+   name: 'Just another text editor',
+   short_name: 'JATE',
+   description: 'Just another text editor!',
+   background_color: '#225ca3',
+   theme_color: '#225ca3',
+   start_url: '/',
+   publicPath: '/',
+   icons: [
+     {
+       src: path.resolve('src/images/logo.png'),
+       sizes: [96, 128, 192, 256, 384, 512],
+       destination: path.join('assets', 'icons'),
+     },
+   ],
+ }),
+ ],
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
